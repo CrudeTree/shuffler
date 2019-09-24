8d2ad43d9acfe6e1907047c8d0d42d4eae8aa708
James Larivee <mr_15th_entertainer@yahoo.com>
2019-09-24 10:41:37 -0600

    Filled out more java docs.

--- src/edu/cnm/deepdive/Shuffler.java
+++ src/edu/cnm/deepdive/Shuffler.java
@@ -2,14 +2,29 @@ package edu.cnm.deepdive;
 
 import java.util.Random;
 
+
+/**
+ * Simple array shuffling class. Since {@link java.util.Arrays} does not implement a {@code shuffle}
+ * method (the {@link java.util.Collections} class does), this addresses that with a basic
+ *implementation of the Fisher-Yates shuffling algorithm.
+ */
 public class Shuffler {
 
   private final Random rng;
 
+
+  /**
+   * Initializes this instance with the provided source of (pseudo-) randomness.
+   * @param rng random number generator
+   */
   public Shuffler(Random rng) {
     this.rng = rng;
   }
 
+  /**
+   * Shuffles the values of an integer array
+   * @param data array to be shuffled.
+   */
   public void shuffle(int[] data) {
     for (int dest = data.length - 1; dest > 0; dest--) {
       int source = rng.nextInt(dest + 1);
@@ -20,6 +35,10 @@ public class Shuffler {
     }
   }
 
+  /**
+   * Shuffles the values of a byte array
+   * @param data
+   */
   public void shuffle(byte[] data) {
     for (int dest = data.length - 1; dest > 0; dest--) {
       int source = rng.nextInt(dest + 1);
@@ -30,6 +49,10 @@ public class Shuffler {
     }
   }
 
+  /**
+   * SHuffles the values of a double array
+   * @param data
+   */
   public void shuffle(double[] data) {
     for (int dest = data.length - 1; dest > 0; dest--) {
       int source = rng.nextInt(dest + 1);
@@ -40,6 +63,10 @@ public class Shuffler {
     }
   }
 
+  /**
+   * Shuffles the values of a float array
+   * @param data array to be shuffled.
+   */
   public void shuffle(float[] data) {
     for (int dest = data.length - 1; dest > 0; dest--) {
       int source = rng.nextInt(dest + 1);
@@ -50,6 +77,10 @@ public class Shuffler {
     }
   }
 
+  /**
+   * Shuffles the values of a char array
+   * @param data array to be shuffled.
+   */
   public void shuffle(char[] data) {
     for (int dest = data.length - 1; dest > 0; dest--) {
       int source = rng.nextInt(dest + 1);
@@ -60,6 +91,11 @@ public class Shuffler {
     }
   }
 
+  /**
+   * Shuffles the values of a generic array
+   * @param data array to be shuffled.
+   * @param <T> array element type.
+   */
   public <T> void shuffle(T[] data) {
     for (int dest = data.length - 1; dest > 0; dest--) {
       int source = rng.nextInt(dest + 1);
